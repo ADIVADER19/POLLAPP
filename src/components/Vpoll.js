a477e245314d6187b8720d06f8611d72777f8b32
SohamX <sohamtalekar7@gmail.com>
2021-12-30 15:48:50 +0530

    Vpoll code css and java, index.html changed

--- src/components/Vpoll.js
+++ src/components/Vpoll.js
@@ -1,17 +1,49 @@
-import React from 'react'
-import {Typography,Tabs,Tab} from '@material-ui/core'
+import React from 'react';
+import'./Vpoll.css';
+import {Typography,Button,makeStyles,createMuiTheme,ThemeProvider}  from '@material-ui/core'
+import { useNavigate } from 'react-router';
+import AddIcon from '@material-ui/icons/Add';
+import VisibilityIcon from '@material-ui/icons/Visibility';
+import { v4 as uuidv4 } from 'uuid';
+import {yellow,black} from '@material-ui/core/colors'
+import { createTheme } from '@material-ui/core/styles'
+
 
 function Vpoll() {
+    let navigate = useNavigate();
+    function createpoll()
+    {
+        const id=uuidv4();
+        navigate("/poll/"+id);
+
+    }
+
     return (
-        <div>
-            <Tabs
-              centered
-              textColor="secondary"
-              indicatorColor="secondary"
-            >
-                <Tab label="ACTIVE POLLS"></Tab>
-                <Tab label="VIEW POLLS"></Tab>
-            </Tabs>
+        <div className='coomtainer'>
+            <div className='actoll'>
+                <h1>Active Polls</h1>
+                <h2>No Active Polls</h2>
+                <div className='polsta'>
+                    <div className='poldet'>Poll Details:
+                    <h3 className='pollin'>poll link:</h3>
+                    <h3 className='poltim'>poll time:</h3>
+                    </div>
+                    <div className='sta'>stats:</div>
+                </div>
+            </div>
+            <div className='clooll'>
+                <h1>Closed Polls</h1>
+                <h2>No Closed Polls</h2>
+                <div className='colsta'>
+                    <div className='poldet'>Poll Details:
+                    <h3 className='poltim'>poll time:</h3>
+                    </div>
+                    <div className='sta'>stats:</div>
+                </div>
+            </div>
+            <div className='crells'>
+            <Button className="cpoll" onClick={createpoll} size="large" variant="contained" endIcon={<AddIcon/>}>CREATE POLL</Button>
+            </div>
         </div>
     )
 }
