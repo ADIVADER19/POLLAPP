d27f068aa6f30faf2b47facc6c3ecd087f6906e6
SohamX <sohamtalekar7@gmail.com>
2021-12-31 12:24:29 +0530

    completed LivepollT.ss and js

--- src/components/LivepollT.js
+++ src/components/LivepollT.js
@@ -1,9 +1,69 @@
 import React from 'react'
+import './LivepollT.css';
 
 function LivepollT() {
     return (
-        <div>
-            boobs
+        <div className='actuallythepage'>
+            <div className='ice'>
+                <div className='poltle'>
+                    <h1>Poll Title</h1>
+                    <h2>Poll description</h2>
+                </div>
+                <div className='quests'>
+                    <div className='question'>
+                        <h3>Questions here in loop</h3>
+                    </div>
+                    <div className='options'>
+                        <h3>Options here in loop</h3>
+                    </div>
+                    <div className='vote'>
+                        Votes = ''
+                    </div>
+                    <div className='options'>
+                        <h3>Options here in loop</h3>
+                    </div>
+                    <div className='vote'>
+                        Votes = ''
+                    </div>
+                </div>
+                <div className='quests'>
+                    <div className='question'>
+                        <h3>Questions here in loop</h3>
+                    </div>
+                    <div className='options'>
+                        <h3>Options here in loop</h3>
+                    </div>
+                    <div className='vote'>
+                        Votes = ''
+                    </div>
+                    <div className='options'>
+                        <h3>Options here in loop</h3>
+                    </div>
+                    <div className='vote'>
+                        Votes = ''
+                    </div>
+                </div>
+                <div className='quests'>
+                    <div className='question'>
+                        <h3>Questions here in loop</h3>
+                    </div>
+                    <div className='options'>
+                        <h3>Options here in loop</h3>
+                    </div>
+                    <div className='vote'>
+                        Votes = ''
+                    </div>
+                    <div className='options'>
+                        <h3>Options here in loop</h3>
+                    </div>
+                    <div className='vote'>
+                        Votes = ''
+                    </div>
+                </div>
+            </div>
+            <div className='fire'>
+                <h1>stats?</h1>
+            </div>
         </div>
     )
 }
