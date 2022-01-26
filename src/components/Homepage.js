a4cd73bbf02f837c8a707dc54ab73534deef25b7
ADIVADER19 <60082645+ADIVADER19@users.noreply.github.com>
2022-01-26 14:55:51 +0530

    added lines in fetch userdata

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -171,11 +171,12 @@ function HomePage() {
                 console.log(data);
                 console.log(tok);
                 window.alert("SUCCESSFULLY LOGGED IN")
-                document.cookie=`soham=${tok}; max-age=4800,path=/,domain='.pollapp-five.vercel.app)`;
-                //props.setTrigger(false)
-                window.location.reload();
                 setTimedPopup(false);
                 userd()
+                //document.cookie=`soham=${tok}; max-age=4800,path=/,domain='.pollapp-five.vercel.app)`;
+                //props.setTrigger(false)
+                window.location.reload();
+                
 			} 
             else if(res.status === 422)
                 {
