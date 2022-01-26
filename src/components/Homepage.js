e6ad382ef209f40007995b7497b007fe3eac1297
ADIVADER19 <60082645+ADIVADER19@users.noreply.github.com>
2022-01-26 12:07:21 +0530

    authenticate

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -171,6 +171,7 @@ function HomePage() {
                 console.log(data);
                 console.log(tok);
                 window.alert("SUCCESSFULLY LOGGED IN")
+                document.cookie=res.cookie.soham
                 //document.cookie=`jwtoken=${tok}; max-age=4800;secure;Path="/")`;
                 //props.setTrigger(false)
                 window.location.reload();
