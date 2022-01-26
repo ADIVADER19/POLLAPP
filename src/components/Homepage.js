b66f0702eede741c24c0e9bb60421ba88bf73a5d
ADIVADER19 <60082645+ADIVADER19@users.noreply.github.com>
2022-01-26 19:47:19 +0530

    added lines in fetch userdata

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -110,9 +110,8 @@ function HomePage() {
 			const res = await fetch(`${link}userdata`, {
 				method: "GET",
 				headers: {
-					Accept: "application/json",
-					"Content-Type": "application/json",
-				},
+                    Authorization: "Bearer " + localStorage.getItem("jwt"),
+                  },
 				credentials: "include",
 			});
 			const data = await res.json();
