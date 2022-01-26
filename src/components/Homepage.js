c4e17d800634af2afb8b05257092b8f86d30bd7e
ADIVADER19 <60082645+ADIVADER19@users.noreply.github.com>
2022-01-26 10:52:40 +0530

    authenticate

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -113,7 +113,7 @@ function HomePage() {
 					Accept: "application/json",
 					"Content-Type": "application/json",
 				},
-				credentials: "include",
+				//credentials: "include",
 			});
 			const data = await res.json();
             
