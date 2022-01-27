c253ee5a7fa018e72e1b4bc16a3a0157563b3b4b
ADIVADER19 <60082645+ADIVADER19@users.noreply.github.com>
2022-01-27 13:37:00 +0530

    changed userdata for profile and pollstu

--- src/components/Vpoll.js
+++ src/components/Vpoll.js
@@ -266,7 +266,7 @@ function Vpoll() {
     const[linktxt,setLinktxt]=useState("Share Link");
     const [op, setOp] = React.useState(false);
     const data = {name:'teacher'};
-    const ENDPOINT = 'https://pollapp281907.herokuapp.com';
+    const ENDPOINT = 'pollapp281907.herokuapp:2000';
     const [modal, setmodal] = useState(false);
     const[deletelobid,setDeletelobid]=useState('');
     const[delMessage,setdelMessage]=useState(false);
