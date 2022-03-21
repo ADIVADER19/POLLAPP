8181ea4303b7c30c66943b2ad1c00277725c57ee
ADIVADER19 <60082645+ADIVADER19@users.noreply.github.com>
2022-03-21 15:09:37 +0530

    changed nav.js

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -167,9 +167,10 @@ function HomePage() {
 			} else if (res.status === 200 || res.status === 201) {
                 window.alert("SUCCESSFULLY LOGGED IN")
                 //localStorage.setItem("jwt", tok);
+                window.location.reload();
                 setTimedPopup(false);
                 userd()
-                window.location.reload();
+                
                 
                 
 			} 
